44b35e7d6d39adcf7321a3f6a525b1f2a9986713
Vadim <vtroshchinskiy@qindel.com>
2019-11-05 16:24:14 +0100

    Minor documentation improvements for QVDLibNXBackend

--- qtclient/qvdclient/backends/qvdlibnxbackend.h
+++ qtclient/qvdclient/backends/qvdlibnxbackend.h
@@ -13,6 +13,30 @@
 #include "socketforwarder.h"
 #include "xserverlauncher.h"
 
+/**
+ * @brief libXcomp based NX backend.
+ *
+ * This backend uses libXcomp to implement NX support. Compared to the process-based
+ * QVDNXBackend, it has much greater access to the internals of NX.
+ *
+ * \par Issues with SSL
+ *
+ * QVD uses a single SSL protected connection to handle its own protocol that is later
+ * handed to libXcomp. Unfortunately, libXcomp doesn't currently have any way of making
+ * it call a function to obtain data, and can't work with a SSL socket, so a workaround
+ * had to be implemented.
+ *
+ * The SocketForwarder class connects two sockets together by reading from one and writing
+ * to another and vice-versa. This is used to connect a SSL socket with a cleartext one, and
+ * then hand the cleartext side to libXcomp.
+ *
+ * \par Initialization sequence:
+ * * Start X server, if needed
+ * * Start local socket listener
+ * * Connect QVD socket (SSL) with local socket (cleartext)
+ * * Hand cleartext side to libXcomp
+ * * Do NX processing.
+ */
 
 class QVDLibNXBackend : public QVDBackend
 {
@@ -34,6 +58,7 @@ private slots:
     // Stage 2: Socket server for the unencrypted socket is available
     void SocketServerReady();
 
+    // Stage 3: Run NX.
     void RunNX();
 private:
     void startNX();
