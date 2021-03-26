52bee2c8d92edabe36d7ecb370fa7b6f92b77b2e
Cristian Villegas <cvillegas@qindel.com>
2021-03-26 17:20:31 -0600

    Run docker with root user to build nx-libs.

--- packaging/docker/centos-8/nx-libs/build.sh
+++ packaging/docker/centos-8/nx-libs/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $HOME && mock --buildsrpm --spec ./nx-libs.spec --sources ./nx-libs-3.5.99.25.tar.gz --resultdir ~/result
-mock --rebuild ~/result/nx-libs-3.5.99.25-0.0build1.el8.src.rpm --resultdir ~/result
-cp -av ~/result/* /builddir/build/RPMS/
+su - nxbuilder -c "cd $HOME && mock --buildsrpm --spec ./nx-libs.spec --sources ./nx-libs-3.5.99.25.tar.gz --resultdir ~/result"
+su - nxbuilder -c "mock --rebuild ~/result/nx-libs-3.5.99.25-0.0build1.el8.src.rpm --resultdir ~/result"
+cp -av /home/nxbuilder/result/* /builddir/build/RPMS/
