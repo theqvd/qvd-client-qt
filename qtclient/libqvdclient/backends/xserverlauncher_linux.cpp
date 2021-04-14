96fae30fbfde1f7c7f71c6767d7520836444d121
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2021-04-14 16:05:47 +0200

    Call xhost to allow the local user to connect

--- qtclient/libqvdclient/backends/xserverlauncher_linux.cpp
+++ qtclient/libqvdclient/backends/xserverlauncher_linux.cpp
@@ -9,12 +9,18 @@
 
 XServerLauncher::XServerLauncher()
 {
-    // On Linux we don't launch anything, so just parse whatever the current display is
-    // and return it back.
+    // On Linux we don't launch the X Server, since it has to be already running.
+    // We instead call xhost to allow NX to connect. Most distributions these days allow
+    // the local user to connect, but some don't.
 
     auto display = qgetenv("DISPLAY");
     auto parts = display.split(':');
     auto disp = QString::fromUtf8( parts[1].split('.')[0] );
+
+    QObject::connect(&m_process, SIGNAL(started()), this, SLOT(processStarted()));
+    QObject::connect(&m_process, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(processFinished(int, QProcess::ExitStatus)));
+    QObject::connect(&m_process, SIGNAL(readyReadStandardError()), this, SLOT(processStderrReady()));
+    QObject::connect(&m_process, SIGNAL(readyReadStandardOutput()), this, SLOT(processStdoutReady()));
 
 
     m_display = disp.toUShort();
@@ -25,7 +31,11 @@ XServerLauncher::XServerLauncher()
 
 
 void XServerLauncher::start() {
-    emit running();
+    QString user = qgetenv("USER");
+    qDebug() << "Allowing user " << user << " to connect to X server";
+
+    m_process.start("xhost", { QString("+SI:localuser:%1").arg(user)});
+    qDebug() << "Started process " << m_process.program() << " with args " << m_process.arguments();
 }
 
 void XServerLauncher::stop() {
@@ -34,27 +44,31 @@ void XServerLauncher::stop() {
 
 void XServerLauncher::processStarted()
 {
-
+    qDebug() << "xhost process started";
 }
 
-void XServerLauncher::processFinished(int __attribute__((unused)) exitCode, __attribute__((unused)) QProcess::ExitStatus exitStatus)
+void XServerLauncher::processFinished(int exitCode, QProcess::ExitStatus exitStatus)
 {
-
+    qDebug() << "xhost process finished with exit code" << exitCode << ", status" << exitStatus;
+    emit running();
 }
 
-void XServerLauncher::processError(QProcess::ProcessError __attribute__((unused)) error)
+void XServerLauncher::processError(QProcess::ProcessError error)
 {
-
+    qDebug() << "xhost process failed with error" << error;
+    emit running();
 }
 
 void XServerLauncher::processStdoutReady()
 {
-
+    QByteArray data = m_process.readAllStandardOutput();
+    qInfo() << "xhost out: " << data;
 }
 
 void XServerLauncher::processStderrReady()
 {
-
+    QByteArray data = m_process.readAllStandardError();
+    qInfo() << "xhost err: " << data;
 }
 
 void XServerLauncher::checkServerIsUp()
