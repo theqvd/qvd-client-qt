d6df996e9c9c983e79e1e4d3708aed3342f30356
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2021-04-22 09:46:32 +0200

    Add tests

---
+++ qtclient/libqvdclient/tests/tst_pathtools.cpp
@@ -0,0 +1,104 @@
+#include <QtTest>
+#include <QCoreApplication>
+#include "helpers/binaryfinder.h"
+#include <QFile>
+#include <QDir>
+#include <QFileInfo>
+
+class tst_PathTools : public QObject
+{
+    Q_OBJECT
+
+public:
+    tst_PathTools();
+    virtual ~tst_PathTools();
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void logDir();
+    void getPulseaudioHome();
+    void getPulseaudioStateDir();
+    void getPulseaudioBaseConfig();
+    void findBin();
+
+private:
+    void verifyBin(const QString &bin);
+};
+
+
+tst_PathTools::tst_PathTools()
+{
+
+}
+
+tst_PathTools::~tst_PathTools()
+{
+}
+
+void tst_PathTools::initTestCase() {
+
+}
+
+void tst_PathTools::cleanupTestCase() {
+
+}
+void tst_PathTools::logDir()
+{
+    QFileInfo logdir(PathTools::getLogDir());
+    QVERIFY( logdir.exists() );
+    QVERIFY( logdir.isDir() );
+    QVERIFY( logdir.isWritable() );
+    QWARN( logdir.absolutePath().toUtf8() );
+}
+
+void tst_PathTools::getPulseaudioHome()
+{
+    QFileInfo pahome(PathTools::getPulseaudioHome());
+    QVERIFY( pahome.exists() );
+    QVERIFY( pahome.isDir() );
+    QVERIFY( pahome.isWritable() );
+    QWARN( pahome.absolutePath().toUtf8() );
+
+}
+
+void tst_PathTools::getPulseaudioStateDir()
+{
+    QFileInfo pastate(PathTools::getPulseaudioStateDir());
+    QVERIFY( pastate.exists() );
+    QVERIFY( pastate.isDir() );
+    QVERIFY( pastate.isWritable() );
+    QWARN( pastate.absolutePath().toUtf8() );
+}
+
+void tst_PathTools::getPulseaudioBaseConfig()
+{
+    QFileInfo pastate(PathTools::getPulseaudioBaseConfig());
+    QVERIFY( pastate.exists() );
+    QVERIFY( !pastate.isDir() );
+    QVERIFY( pastate.isReadable() );
+    QWARN( pastate.absolutePath().toUtf8() );
+}
+
+void tst_PathTools::findBin()
+{
+    verifyBin("pulseaudio");
+    verifyBin("nxproxy");
+    verifyBin("sftp-server");
+
+}
+
+void tst_PathTools::verifyBin(const QString &bin)
+{
+    QString path = PathTools::findBin(bin);
+    QFileInfo info(path);
+
+    QVERIFY( !path.isEmpty() );
+    QVERIFY( info.exists() );
+    QVERIFY( info.isReadable() );
+    QVERIFY( info.isExecutable() );
+}
+
+QTEST_MAIN(tst_PathTools)
+
+#include "tst_pathtools.moc"
