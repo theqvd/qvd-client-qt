fc76af6a6b0d8533850c94bffb7056512ff318bc
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2021-06-21 14:57:47 +0200

    Rename header

--- qtclient/libqvdclient/usbip/usbdevicelist.cpp
+++ qtclient/libqvdclient/usbip/usbdevicelist.cpp
@@ -7,7 +7,7 @@
 #include <tchar.h>
 #include <setupapi.h>
 #include <initguid.h>
-#include "helpers/binaryfinder.h"
+#include "helpers/pathtools.h"
 
 DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE, 0xA5DCBF10L, 0x6530, 0x11D2, 0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED);
 #endif
