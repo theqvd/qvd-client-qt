8d3e383dee09fdeb210814132ead7c77865c1fa1
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2022-07-26 12:13:16 +0200

    More complete update of translation paths

--- qtclient/gui/i18n/QVD_Client_en_US.ts
+++ qtclient/gui/i18n/QVD_Client_en_US.ts
@@ -4,7 +4,7 @@
 <context>
     <name>ConnectionStatistics</name>
     <message>
-	<location filename="../connectionstatistics.ui" line="+14"/>
+        <location filename="../connectionstatistics.ui" line="+14"/>
         <source>Dialog</source>
         <translation></translation>
     </message>
@@ -12,11 +12,12 @@
 <context>
     <name>ListDialog</name>
     <message>
-	<location filename="../listdialog.ui" line="+14"/>
+        <location filename="../listdialog.ui" line="+14"/>
         <source>Select your VM</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../listdialog.cpp" line="+32"/>
         <source>Connect</source>
         <translation></translation>
     </message>
@@ -24,113 +25,140 @@
 <context>
     <name>MainWindow</name>
     <message>
-	<location filename="../mainwindow.ui" line="+14"/>
+        <location filename="../mainwindow.ui" line="+14"/>
         <source>QVD</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+11"/>
+        <location line="+125"/>
         <source>Connect</source>
         <translation></translation>
     </message>
     <message>
+        <location line="-23"/>
         <source>Remember password</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+30"/>
         <source>Restart session</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+39"/>
         <source>Settings</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+14"/>
         <source>Options</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+7"/>
         <source>Enable audio</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+7"/>
         <source>Enable microphone</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+7"/>
         <source>Enable printing</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+7"/>
         <source>Full screen</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+7"/>
         <source>Show bandwidth graph</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+29"/>
         <source>Connectivity</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+9"/>
         <source>Server</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+10"/>
         <source>Connection type</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+39"/>
         <source>Shared Folders</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+16"/>
         <source>Enable shared folders</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+10"/>
         <source>Add</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+13"/>
         <source>Remove</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+15"/>
         <source>Environment Variables</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+14"/>
         <source>USB</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+14"/>
         <source>Enable USB redirection</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+26"/>
         <source>Version</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+14"/>
         <source>Release: (running from source code)
 Git commit: N/A
 Build: N/A</source>
         <translation></translation>
     </message>
     <message>
+        <location line="-377"/>
         <source>Username</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+14"/>
         <source>Password</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+17"/>
         <source>Second Factor</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+364"/>
         <source>&lt;a href=&quot;http://theqvd.com&quot;&gt;© 2022 Qindel Group&lt;/a&gt;</source>
         <translation></translation>
     </message>
@@ -138,27 +166,32 @@ Build: N/A</source>
 <context>
     <name>SSLErrorDialog</name>
     <message>
-	<location filename="../sslerrordialog.ui" line="+14"/>
+        <location filename="../sslerrordialog.ui" line="+14"/>
         <source>Dialog</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+10"/>
         <source>Problems</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+69"/>
         <source>Result of the certificate check</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+13"/>
         <source>Recommendations</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+6"/>
         <source>Details</source>
         <translation></translation>
     </message>
     <message>
+        <location filename="../sslerrordialog.cpp" line="+27"/>
         <source>Accept temporarily</source>
         <translation></translation>
     </message>
@@ -166,19 +199,22 @@ Build: N/A</source>
 <context>
     <name>TotpEnrollment</name>
     <message>
-	<location filename="../totpenrollment.ui" line="+14"/>
+        <location filename="../totpenrollment.ui" line="+14"/>
         <source>Two factor authentication is required</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+8"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Either you or the system administrator enabled two factor authentication for your account.&lt;/p&gt;&lt;p&gt;Please scan the code shown below with your phone using Google Authenticator or a similar tool. Once that is done, enter the code generated by the app into t he &amp;quot;Second Factor&amp;quot; field. You can also enter the code below into your application by hand.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+51"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you are having trouble logging in, the server&apos;s owner provided the following message:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation></translation>
     </message>
     <message>
+        <location line="+10"/>
         <source>(no message has been set)</source>
         <translation></translation>
     </message>
