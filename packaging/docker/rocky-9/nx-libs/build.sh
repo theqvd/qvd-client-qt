7a8a3808e8a2cf0b3db43e264c05201212575e54
Cristian Villegas <cvillegas@qindel.com>
2023-02-21 12:45:03 -0600

    Update package name nx-libs-3.5.99.25-0.0build1.el9.src.rpm

--- packaging/docker/rocky-9/nx-libs/build.sh
+++ packaging/docker/rocky-9/nx-libs/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 su - nxbuilder -c "cd /home/nxbuilder && mock --buildsrpm --spec ./nx-libs.spec --sources ./nx-libs-3.5.99.25.tar.gz --resultdir /home/nxbuilder/result"
-su - nxbuilder -c "mock --rebuild /home/nxbuilder/result/nx-libs-3.5.99.25-0.0build1.el8.src.rpm --resultdir /home/nxbuilder/result"
+su - nxbuilder -c "mock --rebuild /home/nxbuilder/result/nx-libs-3.5.99.25-0.0build1.el9.src.rpm --resultdir /home/nxbuilder/result"
 cp -av /home/nxbuilder/result/* /builddir
 
 gpg --import /root/qvd-pub.gpg
